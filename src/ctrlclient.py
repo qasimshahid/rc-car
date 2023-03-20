eb7450f7cec15e35bb594736b58ee14dae643a9a
Qasim Shahid <qasimshahid4477@gmail.com>
2023-03-20 00:46:58 -0400

    Update ctrlclient.py

--- src/ctrlclient.py
+++ src/ctrlclient.py
@@ -3,8 +3,8 @@ import socket
 print("First, please specify the IP address of the server. Form : 192.168.x.x."
       "\nIf you do not know the IP address, please run \"ifconfig\" on the beaglebone."
       "\nYou should scroll all the way down to the wlan0 section. "
-      "From there, copy the IP address after \"inet\"\n\n Enter the IP: ")
-IP = str(input())
+      "From there, copy the IP address after \"inet\"")
+IP = str(input("Enter IP address: "))
 PORT = 7007
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.connect((IP, PORT))
