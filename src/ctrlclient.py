b894f6c97eab3af291c6a83ee3f705cdf3584c22
Qasim Shahid <qasimshahid4477@gmail.com>
2023-03-22 04:25:19 -0400

    cam + oop

--- src/ctrlclient.py
+++ src/ctrlclient.py
@@ -1,16 +1,15 @@
 import socket
-
-print("First, please specify the IP address of the server. Form : 192.168.x.x."
-      "\nIf you do not know the IP address, please run \"ifconfig / ipconfig\"."
-      "\nYou should scroll all the way down to the wlan0 section. "
-      "From there, copy the IP address after \"inet\"")
 serverIP = str(input("Enter IP address: "))
 print("Using this IP: " + serverIP + "\n")
 port = 7007
 buff = 1024
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((serverIP, port))
-
-message = sock.recv(buff)
-print(f"Message from Server: {message.decode()}")
+while True:
+    message = sock.recv(buff)
+    if message:
+        print(f"Message from Server: {message.decode()}")
+    else:
+        print("Connection terminated")
+        break
 
---
+++ src/ctrlserverclass.py
@@ -0,0 +1,45 @@
+import socket
+import time
+
+
+def get_ip(): # courtesy of stack overflow,
+    # https://stackoverflow.com/questions/166506/finding-local-ip-addresses-using-pythons-stdlib
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        s.connect(('8.8.8.8', 1))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = '127.0.0.1'
+    finally:
+        s.close()
+    return ip
+
+
+class ControllerServer:
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.buff = 1024
+        self.port = 7007
+        self.delay = 0.008 # 125 Hz polling rate, original Xbox One controller, 8 milliseconds
+        self.client = None
+        self.host_ip = get_ip()
+        print("Host IP: " + self.host_ip)
+
+    def establish_connection(self):
+        self.sock.bind((self.host_ip, self.port))
+        self.sock.listen(1)
+        client_socket, client_address = self.sock.accept()
+        self.client = client_socket
+        print(f"Connection established from address {client_address}")
+
+    def close_connection(self):
+        self.sock.close()
+
+    def send_msg(self):
+        i = 0
+        while i < 1000:
+            self.client.send(bytes("Hello!", "utf-8"))
+            time.sleep(self.delay)
+            i = i + 1
+
