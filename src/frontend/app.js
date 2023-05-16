bb2f9f2e014bffbc5a45e22ed57e1bf9ed2ed6df
Qasim Shahid <qasimshahid4477@gmail.com>
2023-05-16 03:15:16 -0400

    button switch
    cam feed switch

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,12 +1,10 @@
-let ipAddress = ""
-
 function changeStream() {
     const currentSrc = document.getElementById('stream').getAttribute('src');
     let newSrc;
     if (currentSrc.endsWith('cam1')) {
-      newSrc = `http://${ipAddress}:8554/cam2`;
+      newSrc = `http://G17:8889/cam2`;
     } else {
-      newSrc = `http://${ipAddress}:8554/cam1`;
+      newSrc = `http://G17:8889/cam1`;
     }
     document.getElementById('stream').setAttribute('src', newSrc);
   }
